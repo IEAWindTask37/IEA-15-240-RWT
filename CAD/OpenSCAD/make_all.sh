ca399b2a63047e48d7586179abc2a1d6da7d9115
Garrett Barter <garrett.barter@nrel.gov>
2021-12-02 07:28:04 -0700

    updating CAD after floating tower and blade tweaks

--- CAD/OpenSCAD/make_all.sh
+++ CAD/OpenSCAD/make_all.sh
@@ -2,32 +2,36 @@
 
 froot="IEA-15-240-RWT"
 float="${froot}_VolturnUS-S"
-fixed_fine="../../WT_Ontology/${froot}_FineGrid.yaml"
-float_fine="../../WT_Ontology/${float}_FineGrid.yaml"
+fixed_yaml="../../WT_Ontology/${froot}.yaml"
+float_yaml="../../WT_Ontology/${float}.yaml"
 openscad="/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD"
 
 # Blade
-python -m windio2cad --input $fixed_fine --output ${froot}_blade.stl --openscad $openscad --blade
+python -m windio2cad --input $fixed_yaml --output ${froot}_blade.stl --openscad $openscad --blade
 /bin/mv intermediate.scad ${froot}_blade.scad
 
 # Tower
-python -m windio2cad --input $fixed_fine --output ${froot}_tower.stl --openscad $openscad --tower
+python -m windio2cad --input $fixed_yaml --output ${froot}_tower.stl --openscad $openscad --tower
 /bin/mv intermediate.scad ${froot}_tower.scad
 
+# Floating Tower
+python -m windio2cad --input $float_yaml --output ${float}_tower.stl --openscad $openscad --tower
+/bin/mv intermediate.scad ${float}_tower.scad
+
 # Monopile
-python -m windio2cad --input $fixed_fine --output ${froot}_monopile.stl --openscad $openscad --monopile
+python -m windio2cad --input $fixed_yaml --output ${froot}_monopile.stl --openscad $openscad --monopile
 /bin/mv intermediate.scad ${froot}_monopile.scad
 
 # Floater
-python -m windio2cad --input $float_fine --output ${float}_floater.stl --openscad $openscad --floater
+python -m windio2cad --input $float_yaml --output ${float}_floater.stl --openscad $openscad --floater
 /bin/mv intermediate.scad ${float}_floater.scad
 
 # Full fixed-bottom
-python -m windio2cad --input $fixed_fine --output ${froot}.stl --openscad $openscad
+python -m windio2cad --input $fixed_yaml --output ${froot}.stl --openscad $openscad
 /bin/mv intermediate.scad ${froot}.scad
 
 # Full floating
-python -m windio2cad --input $float_fine --output ${float}.stl --openscad $openscad
+python -m windio2cad --input $float_yaml --output ${float}.stl --openscad $openscad
 /bin/mv intermediate.scad ${float}.scad
 
 # Compress all of the large files
