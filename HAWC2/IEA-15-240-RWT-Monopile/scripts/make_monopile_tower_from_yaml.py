66635afd01070862fc4cf86acac6391354daa6d0
Jenni Rinker <rink@dtu.dk>
2023-06-08 12:56:56 +0200

    separated monopile and embeddedmonopile

--- HAWC2/IEA-15-240-RWT-Monopile/scripts/make_monopile_tower_from_yaml.py
+++ HAWC2/IEA-15-240-RWT-Monopile/scripts/make_monopile_tower_from_yaml.py
@@ -5,23 +5,26 @@ and one for the non-embedded portion.
 
 Requirements: numpy, matplotlib, pyyaml
 """
-from datetime import date
 from pathlib import Path
 
 import matplotlib.pyplot as plt
-import numpy as np
-from numpy import pi
 
 import _functions as myf
 
 
-save_st = 0  # save the HAWC2 tower and monopile st files?
+save_st = 1  # save the HAWC2 tower and monopile st files?
 
 # paths
 h2dir = Path(__file__).parents[1]  # directory of hawc2 model (one level above)
 yaml_path = h2dir / '../../WT_Ontology/IEA-15-240-RWT.yaml'  # yaml file with turbine definition
 ed_paths = dict(tower=h2dir / '../../OpenFAST/IEA-15-240-RWT-Monopile/IEA-15-240-RWT-Monopile_ElastoDyn_tower.dat',
-                monopile=h2dir / '../../OpenFAST/IEA-15-240-RWT-Monopile/IEA-15-240-RWT-Monopile_ElastoDyn_tower.dat')  # elastodyn file
+                monopile=h2dir / '../../OpenFAST/IEA-15-240-RWT-Monopile/IEA-15-240-RWT-Monopile_ElastoDyn_tower.dat',
+                embeddedmonopile=h2dir / '../../OpenFAST/IEA-15-240-RWT-Monopile/IEA-15-240-RWT-Monopile_ElastoDyn_tower.dat',
+                )  # elastodyn file
+
+# constants
+MUDLINE = -30
+TWR_START = 15
 
 # load the yaml file as nested dictionaries
 yamldict = myf.load_yaml(yaml_path)
@@ -29,7 +32,21 @@ yamldict = myf.load_yaml(yaml_path)
 for bodyname in ['tower', 'embeddedmonopile', 'monopile']:
 
     # get dimensions and material properties
-    stn, out_diam, thick, E, G, rho, outfitting_factor = myf.load_body_properties(bodyname, yamldict, start_from_zero=False)
+    yamlname = 'monopile' if 'monopile' in bodyname else 'tower'
+    stn, out_diam, thick, E, G, rho, outfitting_factor = myf.load_body_properties(yamlname, yamldict, start_from_zero=False)
+
+    # isolate/tweak data if we are monopile or embedded monopile
+    if bodyname == 'monopile':
+        mask = (stn > MUDLINE) & (stn <= TWR_START)  # from -30 (excl) to +15 (incl)
+        stn = stn[mask]
+        out_diam = out_diam[mask]
+        thick = thick[mask]
+        stn[0] = MUDLINE  # correct first station from -29.999 to -30
+    elif bodyname == 'embeddedmonopile':
+        mask = (stn <= MUDLINE)   # from -75 (incl) to -30 (incl)
+        stn = stn[mask]
+        out_diam = out_diam[mask]
+        thick = thick[mask]
 
     # plot outer diameter and thickness for verification
     myf.plot_od_thickness(stn, out_diam, thick, bodyname)
@@ -42,8 +59,6 @@ for bodyname in ['tower', 'embeddedmonopile', 'monopile']:
 
     # plot distributed material properties, ed versus hawc2
     myf.plot_dist_matprops_of_h2(out_arr, ed_st, bodyname)
-
-    plt.show()
 
     if save_st:
         h2_st_path = h2dir / f'data/IEA_15MW_RWT_{bodyname.capitalize()}_st.dat'
